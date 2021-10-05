10fa524eb6e62fed4833f39db53efaa57270f667
oscar369 <oscar77rama@gmail.com>
2021-10-05 11:36:48 +0200

    creada la web inicial

---
+++ Principal.java
@@ -0,0 +1,13 @@
+/**
+ * Principal
+ */
+public class Principal {
+    public static void main(String[] args) {
+        int counter = 1;
+        while (counter <= 1000) {
+            System.out.println(counter);
+            counter = counter + 1;
+        }
+    }
+    
+}
